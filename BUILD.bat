102b5447f6a121e3dd206054d1cd84f39640dc10
Mircea Neacsu <mircea@neacsu.net>
2022-07-22 12:07:45 -0400

    Some cleanup

--- BUILD.bat
+++ BUILD.bat
@@ -4,5 +4,21 @@ rem  Master build script. Execute this script to create all output libraries
 rem  Environment variable DEV_ROOT points to the root of development tree.
 rem
 
-call setup.bat
+rem setup environment for Visual Studio 2017 and later
+
+rem VS2017U2 contains vswhere.exe
+if "%VSWHERE%"=="" set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if not exist "%VSWHERE%" (
+  echo Not found VSWHERE.exe!
+  echo Cannot setup environment.
+  goto :EOF
+)
+
+for /f "usebackq tokens=*" %%i in (`"%VSWHERE%" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+  set VSInstallDir=%%i
+)
+echo Visual studio installation folder is: %VSInstallDir%
+rem "%VSInstallDir%\common7\tools\vsdevcmd.bat"
+call "%VSInstallDir%\common7\tools\vsmsbuildcmd.bat"
+
 msbuild super_app.sln
